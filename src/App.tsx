09cd8e83127cc9ec91989968555e2ed5bf3d8339
Gianluca Bredice <bredicegian@gmail.com>
2024-05-29 12:33:10 -0300

    feature: add skills section to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { SKILLS } from '@/const/skills';
 import { Title } from '@/components/ui/title';
 import me1 from '@/assets/me-1.jpg';
 
@@ -23,6 +24,24 @@ export default function App() {
           alt='Gianluca Bredice Vivarelli'
           className='h-72 w-full rounded-2xl object-cover'
         />
+      </section>
+      <section className='flex flex-col gap-4'>
+        <Title>Habilidades</Title>
+        <span>
+          Tecnologías en las que me especializo y utilizo día a día para
+          trabajar
+        </span>
+        <ul className='flex flex-wrap justify-center gap-6'>
+          {SKILLS.map((skill) => (
+            <li
+              key={skill.name + '_key'}
+              className='flex h-20 w-20 flex-col items-center justify-center gap-2'
+            >
+              <skill.icon size={50} />
+              <small>{skill.name}</small>
+            </li>
+          ))}
+        </ul>
       </section>
     </main>
   );
